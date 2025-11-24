b639e78460b65bb98470b5dbba2f7a797cca63d4
liam1472 <liam@fliight.com.au>
2025-11-25 09:48:22 +1100

    chore: Fix GitHub Pages config for liam1472 account

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,14 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://argus-vts-guide.github.io',
+  url: 'https://liam1472.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/argus-interactive-guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Fliight-Engineering', // Usually your GitHub org/user name.
+  organizationName: 'liam1472', // Usually your GitHub org/user name.
   projectName: 'argus-interactive-guide', // Usually your repo name.
 
   onBrokenLinks: 'throw',
